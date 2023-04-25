862e8fd8fd1a25f1d5f964340847a1cedcf660ce
haophancs <haophan.cs.uit@gmail.com>
2023-04-26 04:19:36 +0700

    update

--- run_t5_tokenizer.py
+++ run_t5_tokenizer.py
@@ -7,7 +7,7 @@ vocab_size = 36_100
 input_sentence_size = None
 
 # Initialize a dataset
-dataset = load_dataset("viwiki", data_files="./data/vie_wikipedia_2021_1M/vie_wikipedia_2021_1M-sentences.txt")
+dataset = data.load_dataset("viwiki", data_files="./data/vie_wikipedia_2021_1M/vie_wikipedia_2021_1M-sentences.txt")
 
 tokenizer = SentencePieceUnigramTokenizer(unk_token="<unk>", eos_token="</s>", pad_token="<pad>")
 
