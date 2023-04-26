ed001060d1479801da43459c57f335fa874b01eb
haophancs <haophan.cs.uit@gmail.com>
2023-04-26 17:23:39 +0700

    update

--- run_t5_mlm_flax.sh
+++ run_t5_mlm_flax.sh
@@ -1,8 +1,8 @@
 python run_t5_mlm_flax.py \
---output_dir="./outputs" \
+--output_dir="./outputs/vi-flan-t5-base" \
 --model_type="t5" \
 --model_name_or_path="google/flan-t5-base" \
---tokenizer_name="./outputs" \
+--tokenizer_name="./outputs/vi-flan-t5-base" \
 --train_file="./data/vie_wikipedia_2021_1M/vie_wikipedia_2021_1M-sentences.txt" \
 --validation_file="./data/vie_wikipedia_2021_300K/vie_wikipedia_2021_300K-sentences.txt" \
 --max_seq_length="512" \
